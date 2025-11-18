337e91a424d039c7ce9abcbc3ce2f4aec514cb77
daehyun kim <101967272+daehyunk1m@users.noreply.github.com>
2025-11-19 01:32:02 +0900

    Feat: createVNode, createElement 함수 내용 추가. 단위테스트 통과 확인

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,49 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (typeof vNode === "string") return document.createTextNode(vNode);
+  if (typeof vNode === "number") return document.createTextNode(String(vNode));
+  if (typeof vNode === "boolean") return document.createTextNode("");
+  if (typeof vNode === "undefined") return document.createTextNode("");
 
-function updateAttributes($el, props) {}
+  if (typeof vNode === "object") {
+    if (vNode === null) return document.createTextNode("");
+
+    if (Array.isArray(vNode)) {
+      const fragment = document.createDocumentFragment();
+      vNode.forEach((child) => {
+        fragment.appendChild(createElement(child));
+      });
+      return fragment;
+    }
+
+    if (typeof vNode.type === "function")
+      throw new Error("컴포넌트는 createElement로 처리할 수 없다.");
+
+    const $el = document.createElement(vNode.type);
+
+    updateAttributes($el, vNode.props);
+
+    vNode.children.forEach((child) => {
+      $el.appendChild(createElement(child));
+    });
+
+    return $el;
+  }
+}
+
+function updateAttributes($el, props) {
+  if (!props) return;
+  Object.entries(props).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.className = value.split(" ").join(" ");
+    } else {
+      $el.setAttribute(key, value);
+    }
+    if (key === "style") {
+      $el.style.cssText = Object.entries(value)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join(";");
+    }
+  });
+}
--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -5,6 +5,12 @@ import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
+  const element = createElement(vNode);
+  console.log(element);
+  // updateElement(element, container);
+  // container.appendChild(element);
+  // setupEventListeners(element);
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  // container.addEventListener("click", (event) => {});
 }
